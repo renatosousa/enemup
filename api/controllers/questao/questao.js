01765cb9a37b50a30f5530b181eab6e952d82c57
Renato Sousa <renatocs@senado.gov.br>
2019-11-19 13:11:31 -0300

    primeiro commit

---
+++ api/controllers/questao/questao.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View questão',
+
+
+  description: 'Exibe uma questão.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/questao/questao'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // All done.
+    return {};
+
+  }
+
+
+};
