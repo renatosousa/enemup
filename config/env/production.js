299a2f6c15adb46920c5c37cf8b86fa2a6948380
renatosousa <natocosta@gmail.com>
2019-11-19 17:04:46 -0300

    production commit

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,12 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'http://localhost:1337',
+       'http://enemup.herokuapp.com',
+       'https://enemup.herokuapp.com',
+       'https://localhost:1337',
+     ],
 
 
     /***************************************************************************
--- config/env/staging.js
+++ config/env/staging.js
@@ -41,10 +41,9 @@ module.exports = Object.assign({}, PRODUCTION_CONFIG, {
 
     onlyAllowOrigins: [
       'http://localhost:1337',
-      // 'https://example-staging.herokuapp.com',
-      // 'http://example-staging.herokuapp.com',
-      // 'https://staging.example.com',
-      // 'http://staging.example.com',
+      'http://enemup.herokuapp.com',
+       'https://enemup.herokuapp.com',
+       'https://localhost:1337',
     ],
     //--------------------------------------------------------------------------
     // /\  Hard-code a staging-only override for allowed origins.
